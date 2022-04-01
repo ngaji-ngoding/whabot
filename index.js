5dd5e8043aabbe2c7a4ff731645379d18a8f7d59
ngaji-ngoding <mahsun.b321@gmail.com>
2022-04-01 10:47:13 +0700

    trying md

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const {
-  makeWALegacySocket,
+  default: makeWASocket,
+  //makeWALegacySocket,
   DisconnectReason,
-  useSingleFileLegacyAuthState,
+  useSingleFileAuthState,
+  //useSingleFileLegacyAuthState,
 } = require("@adiwajshing/baileys");
-const {
-  Boom
-} = require("@hapi/boom");
+//const makeWASocket = require("@adiwajshing/baileys");
+console.log(makeWASocket);
+const { Boom } = require("@hapi/boom");
 
-const {
-  state,
-  saveState
-} = useSingleFileLegacyAuthState("./auth_info.json");
+const { state, saveState } = useSingleFileAuthState("./auth_info.json");
 
 function startBot() {
-  const sock = makeWALegacySocket({
+  const sock = makeWASocket({
     printQRInTerminal: true,
     auth: state,
   });
 
   sock.ev.on("connection.update", (update) => {
     console.log(update);
-    const {
-      connection, lastDisconnect
-    } = update;
+    const { connection, lastDisconnect } = update;
     if (connection === "close") {
       const shouldReconnect = (lastDisconnect.error =
         Boom?.output?.statusCode !== DisconnectReason.loggedOut);
@@ -41,86 +38,111 @@ function startBot() {
     }
   });
 
-  sock.ev.on("creds.update",
-    saveState);
+  sock.ev.on("creds.update", saveState);
+  sock.ev.on("messages.upsert", async ({ messages, type }) => {
+    console.log(messages);
+    let pesan, responseList, responbutton, noHp;
 
-  sock.ev.on("messages.upsert",
-    ({
-      messages, type
-    }) => {
-      console.log(messages);
-      const pesan = messages[0].message.conversation;
-      const responseList = messages[0].message.listResponseMessage;
-      const responseButtons = messages[0].message.buttonsResponseMessage;
-      const noHp = messages[0].key.remoteJid;
-      const menus = [{
-        title: 'menus',
-        rows: [{
-          title: 'kopi hitam',
-          rowId: '1'
-        },
-          {
-            title: 'kopi susu',
-            rowId: '2'
-          },
-          {
-            title: 'omelet',
-            rowId: '3'
-          },
-        ]
-      }];
-      if (!messages[0].key.fromMe && pesan == "ping") {
-        sock.sendMessage(noHp, {
-          text: "pong"
-        });
-      }
-      if (!messages[0].key.fromMe && pesan == "hallo") {
-        sock.sendMessage(noHp, {
-          text: "Haaiiiii"
-        });
-      }
-      if (!messages[0].key.fromMe && pesan == "menu") {
-
-        sock.sendMessage(noHp, {
-          text: 'ini daftar menu kafe',
-          title: 'daftar menu',
-          buttonText: 'tampilkan menu',
-          sections: menus
-        })
-      }
-      if (responseList) {
-        const buttons = [{
-          buttonId: '1',
-          buttonText: {
-            displayText: 'ya'
-          },
-          type: 1
-        },
-          {
-            buttonId: '0',
-            buttonText: {
-              displayText: 'tidak'
+    if (messages[0].message) {
+      pesan = messages[0].message.conversation;
+      responseList = messages[0].message.listResponseMessage;
+      responbutton = messages[0].message.buttonsResponseMessage;
+      noHp = messages[0].key.remoteJid;
+      sock.chatRead(messages[0].key, 1);
+    }
+    if (!messages[0].key.fromMe && pesan == "gabung group") {
+      sock.groupParticipantsUpdate("120363040098805358@g.us", [noHp], "add");
+    }
+    if (!messages[0].key.fromMe && pesan == "fck") {
+      sock.groupParticipantsUpdate(
+        "120363040098805358@g.us",
+        [messages[0].participant],
+        "remove"
+      );
+    }
+    if (!messages[0].key.fromMe && pesan == "ping") {
+      sock.sendMessage(noHp, {
+        text: "pong",
+      });
+    }
+    if (!messages[0].key.fromMe && pesan == "hallo") {
+      sock.sendMessage(noHp, {
+        text: "Haaiiiii",
+      });
+    }
+    if (!messages[0].key.fromMe && pesan == "menu") {
+      const menus = [
+        {
+          title: "menus",
+          rows: [
+            {
+              title: "kopi hitam",
+              rowId: "1",
             },
-            type: 1
-          }];
-        sock.sendMessage(noHp, {
-          text: 'mau pesan lagi?',
-          buttons,
-          headerType: 1
-        })
-      }
-      if (responseButtons) {
-        responseButtons.selectedButtonId == '1'?sock.sendMessage(noHp, {
-          text: 'ini daftar menu kafe',
-          title: 'daftar menu',
-          buttonText: 'tampilkan menu',
-          sections: menus
-        }): sock.sendMessage(noHp, {
-          text: 'terima kasih'
-        })
-      }
-
-    });
+            {
+              title: "kopi susu",
+              rowId: "2",
+            },
+            {
+              title: "omelet",
+              rowId: "3",
+            },
+          ],
+        },
+      ];
+      sock.sendMessage(noHp, {
+        text: "ini daftar menu kafe",
+        title: "daftar menu",
+        buttonText: "tampilkan menu",
+        sections: menus,
+      });
+    }
+    if (responseList) {
+      const buttons = [
+        {
+          buttonId: "1",
+          buttonText: {
+            displayText: "ya",
+          },
+          type: 1,
+        },
+        {
+          buttonId: "0",
+          buttonText: {
+            displayText: "tidak",
+          },
+          type: 1,
+        },
+      ];
+      await sock.sendMessage(noHp, {
+        image: {
+          url: "./img/" + responseList.title + ".jpeg",
+        },
+        caption: responseList.title,
+      });
+      sock.sendMessage(noHp, {
+        text: "mau pesan?",
+        buttons,
+        headerType: 1,
+      });
+    }
+    if (responbutton) {
+      responbutton.selectedButtonId === "1"
+        ? sock.sendMessage(noHp, {
+            text: "silahkan bayar melalui link berikut\n https://link.dana.id/minta/2q3x16jaw9",
+          })
+        : sock.sendMessage(noHp, {
+            text: "terima kasih telah berkunjung",
+          });
+    }
+  });
+  sock.ev.on("group-participants.update", (update) => {
+    if (update.action === "add") {
+      sock.sendMessage(update.id, {
+        text: "selamat datang di group " + update.id,
+      });
+    }
+  });
 }
 
 startBot();
